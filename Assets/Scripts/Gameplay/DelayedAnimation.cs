f1d676ebf3e91248ac1b0e10a3c5401c2b805705
Bortnak <kevin.ledevehat@gmail.com>
2017-01-21 23:43:24 +0100

    Blob prefabs ready for anims + random start animController

---
+++ Assets/Scripts/Gameplay/DelayedAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedAnimation : MonoBehaviour {
+
+	private IEnumerator DelayedAnim()
+	{
+		float time = Random.Range (0.0f, 10.0f);
+		yield return new WaitForSeconds(time);
+		GetComponent<Animator>().SetBool("Playing", true);
+		                                
+	}
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DelayedAnim());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
