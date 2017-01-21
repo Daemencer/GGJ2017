beacd7d45761231e05f136e24c3b05bf7adeace6
Tom GEORGIN <tom.georgin@gmail.com>
2017-01-21 18:35:49 +0100

    Aimer started

---
+++ Assets/Scripts/Gameplay/Aimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aimer : MonoBehaviour
+{
+	#region Methods
+	private void StartAimingProcess()
+	{
+
+	}
+
+
+	private void FinishAimingProcess()
+	{
+
+	}
+	#endregion
+
+	#region Unity Methods
+	private void Start()
+	{
+
+	}
+
+
+	private void Update()
+	{
+
+	}
+	#endregion
+}
